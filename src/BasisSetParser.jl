82629e5379307d0c0e0efa5e113005f6fee3578d
Chunyu Yang <chyyangustc@gmail.com>
2024-12-15 20:27:45 -0500

    rm src

--- src/BasisSetParser.jl
+++
@@ -1,5 +0,0 @@
-# Description: This Julia Code is used to parse the basis set information from a given format of file,
-# Author: Chunyu Yang
-# Email: chunyu.yang@duke.edu, chyyangustc@outlook.com (Permanent Email)
-# Organization: Department of Chemistry, Duke University
-# Date: 2024-12-13
